ff7d95ab9759a3cb44d4379a72adc9b52c9e95eb
Joel Granados <joel.granados@kernel.org>
2025-05-21 11:17:05 +0200

    nixvm: update nixvm test
    Signed-off-by: Joel Granados <joel.granados@kernel.org>

--- examples/nix/basecnf/configuration.nix
+++ examples/nix/basecnf/configuration.nix
@@ -7,31 +7,30 @@
   options = { };
   config = {
 
-    # We are stateless, so we don't use this, but nix will warn without it.
+    # Silence nix warning
     system.stateVersion = "24.11";
 
-    # Shut up nixos warning about missing root
+    # Silence missing root warning
     fileSystems."/" = lib.mkImageMediaOverride {
       fsType = "tmpfs";
       options = [ "mode=0755" ];
     };
 
-    # Disable the remount rw systemd service
+    # Disable early boot remount
     systemd.services.systemd-remount-fs.enable = lib.mkForce false;
 
-    # Don't build the GRUB menu builder script, since we don't need it
-    # here and it causes a cyclic dependency.
+    # Disable Grub
     boot.loader.grub.enable = false;
 
-    # No need to build kernel or initrd
+    # kernel and initrd are not needed
     boot.kernel.enable = false;
     boot.initrd.enable = false;
 
     networking.hostName = "";
-    #networking.dhcpcd.enable = false;
 
-    # Uncomment if you want log output on console
-    #services.journald.console = "/dev/console";
+    # Ensure DNS resolv works
+    networking.networkmanager.enable = true;
+    networking.resolvconf.enable = true;
 
     # The system is static.
     users.mutableUsers = false;
@@ -59,12 +58,6 @@
     services.openssh.settings.PermitRootLogin = "yes";
     services.openssh.settings.PermitEmptyPasswords = "yes";
     security.pam.services.sshd.allowNullPassword = true;
-
-    # For mounting virtiofs passed to run-kernel via --share share:/path/to/place
-    fileSystems."/mnt" = {
-      fsType = "virtiofs";
-      device = "share";
-    };
 
     environment.systemPackages = [ pkgs.coreutils pkgs.python3 pkgs.wget ];
   };
